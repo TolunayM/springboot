6043aba334afa65a8a50be4a78381d1d79c319eb
Tolunay Mutlu <tlnymtlu@gmail.com>
2023-01-17 23:53:35 +0300

    modified

--- src/main/java/com/springtutorial/tutorial/student/StudentService.java
+++ src/main/java/com/springtutorial/tutorial/student/StudentService.java
@@ -1,2 +1,24 @@
-package com.springtutorial.tutorial.student;public class StudentService {
+package com.springtutorial.tutorial.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+@Service
+public class StudentService {
+
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Tolunay",
+                        "tlnymtlu@gmail.com",
+                        LocalDate.of(1999, Month.JANUARY,27),
+                        24
+                )
+        );
+    }
 }
